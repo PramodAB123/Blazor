6e373fb4769f2a09d592c27c188e00a363f1f1f1
PramodAB123 <pramodbkl111@gmail.com>
2025-07-14 00:09:36 +0530

    initail submit

---
+++ WEBapi/WEBapi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WEBapi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var postdata = new PostData
+            {
+                Name = "joe",
+                Age = 16,
+                Address = "hello"
+            };
+            var client =new HttpClient();
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            var json = JsonSerializer.Serialize(postdata);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = client.PostAsync("posts", content).Result;
+            //Console.WriteLine(response);
+            if (response.IsSuccessStatusCode)
+            {
+                var pdata = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(pdata);
+            }
+        }
+    }
+}
---
+++ WEBapi/WEBapi/SendPostResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBapi
+{
+    public class SendPostResponse
+    {
+        public int id { get; set; }
+    }
+}
